8b10659cf7d212066689c0fbba522c7408a2ddaf
Pradeep P <pradeeppackiyam99@gmail.com>
2026-03-06 01:55:47 +0530

    Created Film model and database migration

--- films/models.py
+++ films/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Film(models.Model):
+
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    video = models.FileField(upload_to="films/")
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.titlepython
