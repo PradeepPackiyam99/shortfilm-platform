7dc707aa540a317a43e1ed1630a56eb655075afd
Pradeep P <pradeeppackiyam99@gmail.com>
2026-03-11 00:47:58 +0530

    Day 4 - Created Film API using Django REST Framework

--- films/views.py
+++ films/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Film
+from .serializers import FilmSerializer
 
-# Create your views here.
+
+@api_view(['GET'])
+def get_films(request):
+
+    films = Film.objects.all()
+
+    serializer = FilmSerializer(films, many=True)
+
+    return Response(serializer.data)
