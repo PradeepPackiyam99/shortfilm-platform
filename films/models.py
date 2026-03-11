291314f178617c4168091223899c0a9415e24a9f
Pradeep P <pradeeppackiyam99@gmail.com>
2026-03-12 00:12:14 +0530

    Configure media serving and verify video playback

--- films/models.py
+++ films/models.py
@@ -4,7 +4,7 @@ class Film(models.Model):
 
     title = models.CharField(max_length=200)
     description = models.TextField()
-    video = models.FileField(upload_to="films/")
+    video = models.FileField(upload_to='films/')
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
